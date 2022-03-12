d82f93abecc5ba677f4c22aa07a74c4941ec8fe9
zz0108 <nn12572312@gmail.com>
2022-03-12 16:09:45 +0800

    commit

--- LeetCodeListNodeConverter/Program.cs
+++ LeetCodeListNodeConverter/Program.cs
@@ -3,15 +3,12 @@
 
 using LeetCodeListNodeConverter;
 
-int[] nums = new[] { 2, 3, 4, 5, 6, 7, 8, 9 };
-
-
-var l1 = nums.ConverterToListNode();
-
-while (l1 != null)
-{
-    Console.WriteLine(l1.val);
-    l1 = l1.next;
-}
-
-Console.ReadLine();
+//基本用法，他是個擴充方法，所以直接呼叫就好
+//int[] nums = new[] { 2, 3, 4, 5, 6, 7, 8, 9 };
+//var l1 = nums.ConverterToListNode();
+//while (l1 != null)
+//{
+//    Console.WriteLine(l1.val);
+//    l1 = l1.next;
+//}
+//Console.ReadLine();
